a06a3780b71783206af34ba0c1e2aac62d4e0931
ape06 <zhenhao.li@kellogg.ox.ac.uk>
2023-04-26 11:56:12 +0100

    make mutation test report 100% green for GildedRose

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -82,4 +82,85 @@ public class GildedRoseTest {
         app.updateQuality();
         assertThat(items[0].quality, equalTo(-5));
     }
+
+    @Test
+    public void updateWhenItemIsElixirOfTheMongooseAndQualityIsMoreThanZero() {
+        Item[] items = new Item[] {
+                new Item("Elixir of the Mongoose", 5, 7) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(items[0].quality, equalTo(6));
+    }
+
+    @Test
+    public void updateWhenItemIsElixirOfTheMongooseAndQualityIsLessThanZero() {
+        Item[] items = new Item[] {
+                new Item("Elixir of the Mongoose", 5, -7) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(items[0].quality, equalTo(-7));
+    }
+
+    @Test
+    public void updateWhenItemIsElixirOfTheMongooseAndQualityIsZero() {
+        Item[] items = new Item[] {
+                new Item("Elixir of the Mongoose", 5, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(items[0].quality, equalTo(0));
+    }
+
+    @Test
+    public void boundaryTestWhenSellInIs11AndNameIsBackstagePassesAndQualityIsLessThan50() {
+        Item[] items = new Item[] {
+                new Item("Backstage passes to a TAFKAL80ETC concert", 11, 40),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 40),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 12, 40)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(items[0].quality, equalTo(41));
+        assertThat(items[1].quality, equalTo(42));
+        assertThat(items[2].quality, equalTo(41));
+    }
+
+    @Test
+    public void boundaryTestWhenSellInIs6AndNameIsBackstagePassesAndQualityIsLessThan50() {
+        Item[] items = new Item[] {
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 6, 40),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 7, 40)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(items[0].quality, equalTo(43));
+        assertThat(items[1].quality, equalTo(42));
+        assertThat(items[2].quality, equalTo(42));
+    }
+
+    @Test
+    public void boundaryTestWhenSellInIsBelow0AndNameIsNotAgedOrBackstageAndQualityIsGreaterThan0() {
+        Item[] items = new Item[] {
+                new Item("Elixir of the Mongoose", -1, -1),
+                new Item("Elixir of the Mongoose", -2, 0),
+                new Item("Elixir of the Mongoose", -3, 1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(items[0].quality, equalTo(-1));
+        assertThat(items[1].quality, equalTo(0));
+        assertThat(items[2].quality, equalTo(0));
+    }
+
+    @Test
+    public void boundaryTestWhenSellInIsBelow0AndNameIsAgedBrieAndQualityIsLessThan50() {
+        Item[] items = new Item[] {
+                new Item("Aged Brie", -1, 49),
+                new Item("Aged Brie", -1, 50),
+                new Item("Aged Brie", -1, 51)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(items[0].quality, equalTo(50));
+        assertThat(items[1].quality, equalTo(50));
+        assertThat(items[2].quality, equalTo(51));
+    }
+
+
 }
